0006ab8212275926f140e2ab5aef999bc98dbe5c
Kevin Johnson <55682969+kjohnso3@users.noreply.github.com>
2019-11-25 01:30:55 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,112 @@
+const questionData = {
+  questions: [
+    {
+      question: "Which country won the 2010 World Cup?",
+      answers: [
+        "Spain",
+        "Italy",
+        "Brazil",
+        "France"
+
+      ],
+      correctAnswer: "Spain"
+    },
+    {
+      question: "Which team plays at Old Trafford?",
+      answers: [
+        "FC Barcelona",
+        "Chelsea FC",
+        "Manchester United",
+        "Manchester City"
+      ],
+      correctAnswer: "Manchester United"
+    },
+    {
+      question: "What jersey number does Kylian Mbappe wear?",
+      answers: [
+         "7",
+         "14",
+         "21",
+         "28"
+      ],
+      correctAnswer: "7"
+      
+    },
+    {
+      question: "Which team does Cristiano Ronaldo play for?",
+      answers: [
+        "Manchester United",
+        "Real Madrid",
+        "Juventus",
+        "Inter Milan"
+
+      ],
+      correctAnswer: "Juventus"
+    },
+    {
+      question: "How many points is a win worth?",
+      answers: [
+        "1",
+        "2",
+        "3",
+        "4"
+      ],
+      correctAnswer: "3"
+    },
+    {
+      question: "Lebron James is a part-owner of which team?",
+      answers: [
+        "Los Angeles Galaxy",
+        "Liverpool FC",
+        "Newcastle United",
+        "FC Bayern Munich"
+      ],
+      correctAnswer: "Liverpool FC"
+    },
+      {
+      question: "Who is the manager of Chelsea FC?",
+      answers: [
+        "LAFC",
+        "Chelsea FC",
+        "Arsenal FC",
+        "Villareal"
+
+      ],
+      correctAnswer: "Chelsea FC"
+    },
+    {
+      question: "Which MLS team is based out of Seattle?",
+      answers: [
+        "Seattle Seahawks",
+        "Seattle Sounders",
+        "Seattle Storm",
+        "Seattle Rain"
+      ],
+      correctAnswer: "Seattle Sounders"
+    },
+    {
+      question: "Lionel Messi plays for which team?",
+      answers: [
+        "FC Barcelona",
+        "Boca Juniors",
+        "Real Madrid",
+        "Manchester City"
+      ],
+      correctAnswer: "FC Barcelona"
+      
+    },
+    {
+      question: "Which city was the 2019 Champions League Final played in?",
+      answers: [
+        "Rome",
+        "Madrid",
+        "Paris",
+        "London"
+
+      ],
+      correctAnswer: "Madrid"
+    },
+  ],
+  currentQuestion: 0,
+  score: 0,
+};
